5fee8a45766175c738a26dbeb3d6a1d5b2711ca2
Timishot <timidotcom58@gmail.com>
2024-03-05 22:51:40 +0100

    fix

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,11 +1,12 @@
 const getFullResponseFromAPI = (success) => new Promise((resolve, reject) => {
-  setTimeout(() => {
     if (success) {
       resolve({ status: 200, body: 'Success' });
     } else {
       reject(new Error('The fake API is not working currently'));
     }
-  }, 10000);
 });
 
 export default getFullResponseFromAPI;
+
+
+
