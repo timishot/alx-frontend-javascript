eeaa029ec418fa3ba62dfaac2b76534fbd653f8c
Timishot <timidotcom58@gmail.com>
2024-03-05 22:19:59 +0100

    check if a object is a instance of a promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,11 @@
-const getResponseFromAPi = () => new Promise((resolve, reject) => {
-    // Sitimulating an asynchronous API call, e.g., fetching data
-    setTimeout(()=>{
-        const data = { message: "API resonse data" };
+const getResponseFromAPi = () => new Promise((resolve) => {
+  // Sitimulating an asynchronous API call, e.g., fetching data
+  setTimeout(() => {
+    const data = { message: 'API resonse data' };
 
-        //Simulating a successful resonse
-        resolve(data);
-    }, 1000)
-})
+    // Simulating a successful resonse
+    resolve(data);
+  }, 1000);
+});
 
-export default getResponseFromAPi
+export default getResponseFromAPi;
