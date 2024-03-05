3d09c355ecf4285df9d0351048f6a6fe05bc402a
Timishot <timidotcom58@gmail.com>
2024-03-06 00:36:03 +0100

    fix

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,22 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const [photoResponse, userResponse] = await Promise.all([
+      uploadPhoto(),
+      createUser(),
+    ]);
+
+    return {
+      photo: photoResponse,
+      user: userResponse,
+    };
+  } catch (error) {
+    // Handle errors here
+    console.error(error);
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
