3443304c6812643238e480c90ff37926a929069b
Timishot <timidotcom58@gmail.com>
2024-03-06 12:11:01 +0100

    add

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,17 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-const handleProfileSignup = (firstName, lastName, fileName) => {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(fileName);
 
-  // Using Promise.allSettled to handle both promises
   return Promise.allSettled([userPromise, photoPromise])
     .then((results) => results.map((result) => ({
       status: result.status,
       value: result.status === 'fulfilled' ? result.value : result.reason,
-    })))
-    .catch((error) => [{ status: 'rejected', value: error.message }]);
-};
-
-export default handleProfileSignup;
+    })));
+}
