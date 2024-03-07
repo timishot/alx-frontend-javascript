c349f6f3763b67a4bb287aab387533c74aaeaf36
Timishot <timidotcom58@gmail.com>
2024-03-07 23:17:17 +0100

    fix

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,48 +1,48 @@
-import Currency from "./3-currency";
+import Currency from './3-currency';
 
-export default class Pricing{
-    constructor(amount, currency){
-        this._amount = 0
-        this.Currency = new Currency("", "")
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = 0;
+    this.Currency = new Currency('', '');
 
-        this.amount = amount;
-        this.currency = currency;
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  // Getter and setter for 'amount'
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    const parsedAmount = parseFloat(newAmount);
+    if (!Number.isNaN(parsedAmount)) {
+      this._amount = parsedAmount;
+    } else {
+      throw new TypeError('Amount must be a number');
     }
+  }
 
-    // Getter and setter for 'amount'
-    get amount() {
-        return this._amount;
-    }
+  // Getter and setter for 'currency'
+  get currency() {
+    return this._currency;
+  }
 
-    set amount(newAmount) {
-        const parsedAmount = parseFloat(newAmount);
-        if (!isNaN(parsedAmount)) {
-            this._amount = parsedAmount;
-        } else {
-            throw new TypeError("Amount must be a number");
-        }
+  set currency(newCurrency) {
+    if (newCurrency instanceof Currency) {
+      this._currency = newCurrency;
+    } else {
+      throw new TypeError('Currency must be an instance of the Currency class');
     }
+  }
 
-    // Getter and setter for 'currency'
-    get currency() {
-        return this._currency;
-    }
+  // Method to display full price information
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
 
-    set currency(newCurrency) {
-        if (newCurrency instanceof Currency) {
-            this._currency = newCurrency;
-        } else {
-            throw new TypeError("Currency must be an instance of the Currency class");
-        }
-    }
-
-    // Method to display full price information
-    displayFullPrice() {
-        return `${this._amount} ${this._currency.name} (${this._currency.code})`;
-    }
-
-    // Static method to convert price based on conversion rate
-    static convertPrice(amount, conversionRate){
-        return (amount * conversionRate)
-    }
+  // Static method to convert price based on conversion rate
+  static convertPrice(amount, conversionRate) {
+    return (amount * conversionRate);
+  }
 }
