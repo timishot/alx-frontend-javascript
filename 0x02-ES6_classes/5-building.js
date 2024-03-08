0f7395ec0627c2ced5dfc9b0cd8807f394a15aec
Timishot <timidotcom58@gmail.com>
2024-03-08 20:49:25 +0100

    fix

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,23 +1,14 @@
 export default class Building {
-  constructor(sqft) {
+  constructor(sqft = 0) {
+    if (this.constructor !== Building
+        && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(value) {
-    const parsedSqft = parseFloat(value);
-    if (!Number.isNaN(parsedSqft)) {
-      this._sqft = parsedSqft;
-    } else {
-      throw new TypeError('Amount must be a number');
-    }
-  }
-
-  evacuationWarningMessage() {
-    // eslint-disable-next-line class-methods-use-this
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
