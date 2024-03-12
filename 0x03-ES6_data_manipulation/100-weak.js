e455c4ee818f92d11184311f121f9543790314a6
Timishot <timidotcom58@gmail.com>
2024-03-12 14:48:53 +0100

    working on weaakMap

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,16 @@
+const weakMap = new WeakMap();
+
+const queryAPI = (endpoint) => {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 1);
+  } else {
+    const count = weakMap.get(endpoint) + 1;
+    weakMap.set(endpoint, count);
+
+    if (count >= 5) {
+      throw new Error('Endpoint load is high');
+    }
+  }
+};
+
+export { queryAPI, weakMap };
